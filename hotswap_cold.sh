6542c0db39437049b53a4d5c91b088196c60302f
ra <40503841+RadiantAeon@users.noreply.github.com>
2024-12-31 08:13:26 +0100

    ensure ownership of tower file

--- hotswap_cold.sh
+++ hotswap_cold.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 solana-validator -l /solana/ledger set-identity --require-tower /solana/staked_validator_identity.json
+sudo chown solana -R /solana/ledger
 ln -sf /solana/staked_validator_identity.json /solana/validator_identity.json
