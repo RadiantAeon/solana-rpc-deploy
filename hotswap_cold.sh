ff131b7c9c36b7de2ce6067a63e15bc96391a30e
ra <40503841+RadiantAeon@users.noreply.github.com>
2025-02-12 15:45:04 -0500

    afsd

--- hotswap_cold.sh
+++ hotswap_cold.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-solana-validator -l /solana/ledger set-identity --require-tower /solana/staked_validator_identity.json
+/solana/jito-solana/target/release/agave-validator -l /solana/ledger set-identity --require-tower /solana/staked_validator_identity.json
 sudo chown solana -R /solana/ledger
 ln -sf /solana/staked_validator_identity.json /solana/validator_identity.json
