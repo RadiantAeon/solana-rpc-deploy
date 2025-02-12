b7712b19c951dd6d0df1f21cb6d34cd79523eb09
ra <40503841+RadiantAeon@users.noreply.github.com>
2025-02-12 15:40:18 -0500

    update hotswap script

--- hotswap_hot.sh
+++ hotswap_hot.sh
@@ -2,7 +2,12 @@
 
 # example script of the above steps - change IP obviously
 # copied from https://pumpkins-pool.gitbook.io/pumpkins-pool
+if [ "$#" -ne 1 ]; then
+    echo "Not enough params: run this script with the following args: COLD_SERVER_SSH"
+    exit 1;
+fi
 solana-validator -l /solana/ledger wait-for-restart-window --min-idle-time 2 --skip-new-snapshot-check
 solana-validator -l /solana/ledger set-identity /solana/unstaked_validator_identity.json
 ln -sf /solana/unstaked_validator_identity.json /solana/validator_identity.json
-scp /solana/ledger/tower-1_9-$(solana-keygen pubkey /solana/staked_validator_identity.json).bin sol@68.100.100.10:/solana/ledger
+scp /solana/ledger/tower-1_9-$(solana-keygen pubkey /solana/staked_validator_identity.json).bin $1:/solana/ledger
+ssh $1 'bash -s' < hotswap_cold.sh
