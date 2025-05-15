a64ce47ae53af286a26b5646747ee4525e09a78f
ra <40503841+RadiantAeon@users.noreply.github.com>
2025-05-15 09:55:38 -0400

    change ordering of script + use guaranteed binary

--- hotswap_hot.sh
+++ hotswap_hot.sh
@@ -6,8 +6,8 @@ if [ "$#" -ne 1 ]; then
     echo "Not enough params: run this script with the following args: COLD_SERVER_SSH"
     exit 1;
 fi
-solana-validator -l /solana/ledger wait-for-restart-window --min-idle-time 2 --skip-new-snapshot-check
-solana-validator -l /solana/ledger set-identity /solana/unstaked_validator_identity.json
-ln -sf /solana/unstaked_validator_identity.json /solana/validator_identity.json
+/solana/jito-solana/target/release/agave-validator -l /solana/ledger wait-for-restart-window --min-idle-time 2 --skip-new-snapshot-check
 scp /solana/ledger/tower-1_9-$(solana-keygen pubkey /solana/staked_validator_identity.json).bin $1:/solana/ledger
+/solana/jito-solana/target/release/agave-validator -l /solana/ledger set-identity /solana/unstaked_validator_identity.json
+ln -sf /solana/unstaked_validator_identity.json /solana/validator_identity.json
 ssh $1 'bash -s' < hotswap_cold.sh
