451af7d18f4dd833e1491755984d74d1c225f4d2
ra <40503841+RadiantAeon@users.noreply.github.com>
2025-09-20 19:27:56 -0400

    firedancer hotswap scripts

---
+++ hotswap_cold_firedancer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/solana/firedancer/build/native/gcc/bin/fdctl set-identity --config /solana/firedancer/firedancer_config.toml /solana/staked_validator_identity.json
+sudo chown solana -R /solana/ledger
+ln -sf /solana/staked_validator_identity.json /solana/validator_identity.json
--- hotswap_hot.sh
+++ hotswap_hot.sh
@@ -2,12 +2,23 @@
 
 # example script of the above steps - change IP obviously
 # copied from https://pumpkins-pool.gitbook.io/pumpkins-pool
-if [ "$#" -ne 1 ]; then
-    echo "Not enough params: run this script with the following args: COLD_SERVER_SSH"
+if [ "$#" -ne 3 ]; then
+    echo "Not enough params: run this script with the following args: COLD_SERVER_SSH SOURCE_VARIANT DEST_VARIANT"
+    echo "Variant strings are: \"fd\" and \"agave\""
     exit 1;
 fi
+
 /solana/jito-solana/target/release/agave-validator -l /solana/ledger wait-for-restart-window --min-idle-time 2 --skip-new-snapshot-check
 ln -sf /solana/unstaked_validator_identity.json /solana/validator_identity.json
 scp /solana/ledger/tower-1_9-$(solana-keygen pubkey /solana/staked_validator_identity.json).bin $1:/solana/ledger
-/solana/jito-solana/target/release/agave-validator -l /solana/ledger set-identity /solana/unstaked_validator_identity.json
-ssh $1 'bash -s' < hotswap_cold.sh
+if [$2 == "agave"]; then
+    /solana/jito-solana/target/release/agave-validator -l /solana/ledger set-identity /solana/unstaked_validator_identity.json
+else
+    /solana/firedancer/build/native/gcc/bin/fdctl set-identity --config /solana/firedancer/firedancer_config.toml /solana/unstaked_validator_identity.json
+fi
+ln -sf /solana/unstaked_validator_identity.json /solana/validator_identity.json
+if [$3 == "agave"]; then
+    ssh $1 'bash -s' < hotswap_cold_agave.sh
+else
+   ssh $1 'bash -s' < hotswap_cold_firedancer.sh 
+fi
