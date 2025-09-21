3ea8821204576ba62e819dba17010257e1d1a4af
ra <40503841+RadiantAeon@users.noreply.github.com>
2025-09-20 20:15:38 -0400

    bash is not a real language chat

--- hotswap_hot.sh
+++ hotswap_hot.sh
@@ -8,12 +8,12 @@ if [ "$#" -ne 3 ]; then
     exit 1;
 fi
 
-if ([ $2 != "agave" ] && [ $2 != "fd" ]) || ([ $3 != "agave" ] && [ $3 != "fd" ]); then
+if ([ "$2" != "agave" ] && [ "$2" != "fd" ]) || ([ "$3" != "agave" ] && [ "$3" != "fd" ]); then
     echo "Variant strings are: 'fd' and 'agave'"
     exit 1;
 fi
 
-if [ $2 = "fd" ] && ([ ! -f /solana/firedancer/build/native/gcc/bin/fdctl ] || [ ! -f /solana/firedancer/firedancer_config.toml ]); then
+if [ "$2" = "fd" ] && ([ ! -f /solana/firedancer/build/native/gcc/bin/fdctl ] || [ ! -f /solana/firedancer/firedancer_config.toml ]); then
     echo "Did not find /solana/firedancer/build/native/gcc/bin/fdctl and/or /solana/firedancer/firedancer_config.toml! Are you sure you're running firedancer locally?"
     exit 1;
 fi
@@ -21,12 +21,12 @@ fi
 /solana/jito-solana/target/release/agave-validator -l /solana/ledger wait-for-restart-window --min-idle-time 2 --skip-new-snapshot-check
 ln -sf /solana/unstaked_validator_identity.json /solana/validator_identity.json
 scp /solana/ledger/tower-1_9-$(solana-keygen pubkey /solana/staked_validator_identity.json).bin $1:/solana/ledger
-if [ $2 == "agave" ]; then
+if [ "$2" == "agave" ]; then
     /solana/jito-solana/target/release/agave-validator -l /solana/ledger set-identity /solana/unstaked_validator_identity.json
 else
     /solana/firedancer/build/native/gcc/bin/fdctl set-identity --config /solana/firedancer/firedancer_config.toml /solana/unstaked_validator_identity.json
 fi
-if [ $3 == "agave" ]; then
+if [ "$3" == "agave" ]; then
     ssh $1 'bash -s' < hotswap_cold_agave.sh
 else
     ssh $1 'bash -s' < hotswap_cold_firedancer.sh 
