d2dc22387918e15bb49d7fba757e04c45359d40c
ra <40503841+RadiantAeon@users.noreply.github.com>
2025-09-29 12:45:41 -0400

    firedancer requires force

--- hotswap_cold_firedancer.sh
+++ hotswap_cold_firedancer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/solana/firedancer/build/native/gcc/bin/fdctl set-identity --config /solana/firedancer/firedancer_config.toml /solana/staked_validator_identity.json --require-tower
+/solana/firedancer/build/native/gcc/bin/fdctl set-identity --config /solana/firedancer/firedancer_config.toml /solana/staked_validator_identity.json --require-tower --force
 sudo chown solana -R /solana/ledger
 ln -sf /solana/staked_validator_identity.json /solana/validator_identity.json
